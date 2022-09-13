b3c64735f2ebddea1c76689aa5e453c0fb07be1c
NahuelSaucedo <nahuel.saucedo@alumnos.frgp.utn.edu.ar>
2022-09-13 19:04:54 -0300

    Creada La clase Articulos negocio y la capacidad de listar articulos

---
+++ Controlador/ArticulosNegocio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+namespace Controlador
+{
+    class ArticulosNegocio
+    {
+        public List<Articulos> listar() {
+        List<Articulos> Lista  = new List<Articulos>();
+            AccesoDatos datos = new AccesoDatos();
+            datos.setConsulta("Select A.Id, A.Codigo, A.Nombre, A.Descripcion, A.IdMarca, A.IdCategoria, A.ImagenUrl, A.precio  from Articulos as A");
+            datos.EjecutarLectura();
+            while (datos.Lector.Read())
+            {
+                Articulos aux = new Articulos();
+                aux.Id = (int)datos.Lector["Id"];
+                aux.Codigo = (string)datos.Lector["Codigo"];
+                aux.Nombre = (string)datos.Lector["Nombre"];
+                aux.Descripcion = (string)datos.Lector["Descripcion"];
+                aux.IdMarca = (int)datos.Lector["IdMarca"];
+                aux.idCategoria= (int)datos.Lector["IdCategoria"];
+                aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                aux.Precio = (decimal)datos.Lector["Precio"];
+                Lista.Add(aux);
+            }
+                
+            return Lista;
+    }
+
+
+
+    }
+}
