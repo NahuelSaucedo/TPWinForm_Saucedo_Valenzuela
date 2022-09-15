e5452bfce68347818f6534fe1e320f091d1149cd
NahuelSaucedo <nahuel.saucedo@alumnos.frgp.utn.edu.ar>
2022-09-15 17:34:06 -0300

    TEST

--- Modelo/Articulo.cs
+++ Modelo/Articulo.cs
@@ -11,17 +11,17 @@ namespace Modelo
     public class Articulo
     {
         //public int Id { get; set; }
-        public string  Codigo { get; set; }
+        public string Codigo { get; set; }
         public string Nombre { get; set; }
         [DisplayName("Descripción")]
-        public string  Descripcion { get; set; }
+        public string Descripcion { get; set; }
         //public int IdMarca { get; set; }
         //public int idCategoria { get; set; }
-        public string ImagenUrl { get; set;}
+        public string ImagenUrl { get; set; }
         public decimal Precio { get; set; }
         public Marca Marca { get; set; }
         [DisplayName("Categoría")]
         public Categoria Categoria { get; set; }
-
+        public int test { get; set; }
     }
 }
